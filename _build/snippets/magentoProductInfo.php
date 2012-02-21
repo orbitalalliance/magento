dc43e776d8634ac1bd5348e606634d933433ca1b
steve <steve@e4b04856-0ad7-4d0b-8c2c-cef216831704>
2012-02-21 11:38:41 +0000

    OK, product info from sku working, lets try a build
    git-svn-id: svn://bitez.hobby-site.com:40501/components/trunk/freelance/orbital/oa_magento@2324 e4b04856-0ad7-4d0b-8c2c-cef216831704

---
+++ _build/snippets/magentoProductInfo.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Magento e-commerce interface (oa_magento)
+ *
+ * @author S. Hamblett <steve.hamblett@linux.com>
+ * For Orbital Alliance
+ *
+ * @package  oa_magento
+ * 
+ * Parameters :-
+ * 
+ * skuid                - A single SKU identifier or a comma seperated list of SKU's
+ * wrapperTpl           - An outer wrapper template for results returned, defaults to magentoWrapper
+ * productTpl           - A template for an individual product, defaults to magentoProduct
+ * categoryTpl          - A template for individual categories, defaults to magentoCategory
+ * categoryWrapperTpl   - A template for the product categories, defaults to magentoCategoryWrapper
+ * sortby               - Sortby field
+ * sortdir              - Direction to sort, defaults to ASC
+ * limit                - Limit to, defaults to 10
+ * 
+ * 
+ * Placeholders, all prefixed with oa_magento :-
+ * 
+ * Single 
+ * sku          - The SKU
+ * product_id   - The product Id
+ * name         - Product name
+ * set          - Product set
+ * type         - Product type
+ * price        - Product price
+ * weight       - Product weight
+ * model        - Product model
+ * manufacturer - Product manufacturer
+ * cost         - Product cost
+ * description  - Product description
+ * 
+ * Groups
+ * productlist  - A list of the returned products as themed by the magentoProduct template
+ * categorylist  - A list of the returned categories as themed by the magentoCategory template
+ */
+
+/* Initialise our parameter set */
+$skuid = (!empty($skuid)) ? explode(',', $skuid) : 0;
+$wrapperTpl = !empty($wrapperTpl) ? $wrapperTpl : 'magentoWrapper';
+$categoryWrapperTpl = !empty($categoryWrapperTpl) ? $categoryWrapperTpl : 'magentoCategoryWrapper';
+$productTpl = !empty($itemTpl) ? $productTpl : 'magentoProduct';
+$categoryTpl = !empty($categoryTpl) ? $categoryTpl : 'magentoCategory';
+$limit = isset($limit) ? (integer) $limit : 10;
+$sortby = isset($sortby) ? $sortby : 'SKU';
+$sortdir = isset($sortdir) ? $sortdir : 'ASC';
+
+/* Create the SOAP proxy */
+$proxy = new SoapClient($WSDLURL);
+$sessionId = $proxy->login($apiUser, $apiKey);
+
+/* Get the products from the SKUID(s) */
+$output = "";
+$categoryOutput = "";
+foreach ( $skuid as $aSkuid ) {
+    
+    $productInfo = $proxy->call($sessionId, 'product.info', $aSkuid);
+    $modx->toPlaceholders($productInfo, 'oa_magento' );
+    $output .= $modx->getChunk($productTpl);
+    $categories = $productInfo['categories'];
+    if (!empty($categories) ) {
+        
+        foreach ( $categories as $category ) {
+            $modx->toPlaceholder('category', $category, 'oa_magento');
+            $categoryOutput .= $modx->getChunk($categoryTpl);            
+        } 
+        
+    } else {
+        
+        $categoryOutput = "No Categories defined";
+    }
+        
+    $modx->toPlaceholder('categorylist', $categoryOutput, 'oa_magento');    
+}
+
+/* Set the product list placeholder */
+$modx->toPlaceholder('productlist', $output, 'oa_magento');
