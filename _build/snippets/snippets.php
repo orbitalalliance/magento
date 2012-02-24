e499a3adf77f827c768a49a701792ed9cade1878
steve <steve@e4b04856-0ad7-4d0b-8c2c-cef216831704>
2012-02-24 09:32:10 +0000

    Add id's, see if this helps with removal bug
    git-svn-id: svn://bitez.hobby-site.com:40501/components/trunk/freelance/orbital/oa_magento@2344 e4b04856-0ad7-4d0b-8c2c-cef216831704

--- _build/snippets/snippets.php
+++ _build/snippets/snippets.php
@@ -9,12 +9,14 @@
 $snippets = array();
 
 $s = $modx->newObject('modSnippet');
+$s->set('id',1);
 $s->set('name', 'magentoProductInfo');
 $s->set('description', 'A Magento store product snippet for MODx Revolution. Gets information for products,see the properties for set up values and the snippet itself for parameters and placeholders.');
 $s->set('snippet', file_get_contents($sources['snippets'] . 'magentoProductInfo.php'));
 $snippets[] = $s;
 
 $s = $modx->newObject('modSnippet');
+$s->set('id',2);
 $s->set('name', 'magentoProductInfoFiltered');
 $s->set('description', 'A Magento store product snippet for MODx Revolution. Gets information for products,see the properties for set up values and the snippet itself for parameters and placeholders. Allows filtering as supplied by the Magento API.');
 $s->set('snippet', file_get_contents($sources['snippets'] . 'magentoProductInfoFiltered.php'));
